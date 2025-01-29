b0219b417e20dcae3aa4ddc555472024e8ff3746
npcoiner <nicholascoiner@gmail.com>
2025-01-28 17:38:43 -0800

    added window display in main

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,14 @@
 const std = @import("std");
+const rl = @import("raylib");
 
-pub fn main() !void {}
+pub fn main() !void {
+    rl.initWindow(1280, 720, "zig raylib example");
+    defer rl.closeWindow();
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.sky_blue);
+    }
+}
